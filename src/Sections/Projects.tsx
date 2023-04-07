826b8df4b54f04269cd7900f35b9c61f42244449
brandonjmarquez <60440397+brandonjmarquez@users.noreply.github.com>
2023-04-07 14:34:23 -0500

    Changed project pics and added urls for hosted projects.

--- src/Sections/Projects.tsx
+++ src/Sections/Projects.tsx
@@ -7,29 +7,32 @@ const Projects = () => {
         <h3 className="subtitle">Projects</h3>
         <div className="grid-container">
           <div className="project-container">
-            <a href="https://github.com/brandonjmarquez/piano-roll" target="_blank">
+            <a href="https://piano-roll.vercel.app" target="_blank">
               <img src="/Website/projects/piano.png" className="project-pic" loading="lazy"></img>
             </a>
-            <p className="subtext"><strong>Recordable Piano Instrument</strong></p>
+            <p className="subtext"><strong><a href="https://piano-roll.vercel.app" target="_blank">Recordable Piano Instrument</a></strong></p>
             <p className="proj-desc">This is an in browser piano instrument that can be played and recorded with a computer keyboard. It is made using TypeScript and React.js. Recordings can be exported and used in any modern DAW.</p>
-            <a href="https://github.com/brandonjmarquez/piano-roll">Github Page</a>
+            <a href="https://github.com/brandonjmarquez/piano-roll" target="_blank">Github</a>
           </div>
+
           <div className="project-container">
-            <a href="https://github.com/brandonjmarquez/Website" target="_blank">
+            <a href="https://brandonjmarquez.github.io/Website/">
               <img src="/Website/projects/website.png" className="project-pic" loading="lazy"></img>
             </a>
-            <p className="subtext"><strong>This Website</strong></p>
+            <p className="subtext"><strong><a href="https://brandonjmarquez.github.io/Website/">This Website</a></strong></p>
             <p className="proj-desc">My website, made with TypeScript and React.js. </p>
-            <a href="https://github.com/brandonjmarquez/Website">Github Page</a>
+            <a href="https://github.com/brandonjmarquez/Website" target="_blank">Github</a>
           </div>
+
           <div className="project-container">
             <a href="https://car-dealership-woad.vercel.app" target="_blank">
               <img src="/Website/projects/car-dealership.png" className="project-pic" loading="lazy"></img>
             </a>
-            <p className="subtext"><strong>Car Dealership Website</strong></p>
-            <p className="proj-desc">A mock car dealership website. Made with TypeScript, React.js, TalwindCSS, and Emotion.</p>
-            <a href="https://github.com/brandonjmarquez/car-dealership">Github Page</a>
+            <p className="subtext"><strong><a href="https://car-dealership-woad.vercel.app" target="_blank">Car Dealership Website</a></strong></p>
+            <p className="proj-desc">A mock car dealership website. Made with TypeScript and React.js. Styled with TalwindCSS and Emotion.</p>
+            <a href="https://github.com/brandonjmarquez/car-dealership">Github</a>
           </div>
+
           <div className="project-container">
             <a>
               <img src="/Website/projects/black.png" className="project-pic" loading="lazy"></img>
