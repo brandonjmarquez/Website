9e5e54d5ebfef6537a67779508742512b8f49a28
brandonjmarquez <60440397+brandonjmarquez@users.noreply.github.com>
2023-06-03 00:36:28 -0500

    Added Blab Times to my projects section.

--- src/Sections/Projects.tsx
+++ src/Sections/Projects.tsx
@@ -12,7 +12,7 @@ const Projects = () => {
             </a>
             <p className="subtext"><strong><a href="https://piano-roll.vercel.app" target="_blank">Recordable Piano Instrument</a></strong></p>
             <p className="proj-desc">This is an in browser piano instrument that can be played and recorded with a computer keyboard. It is made using TypeScript and React.js. Recordings can be exported and used in any modern DAW.</p>
-            <a href="https://github.com/brandonjmarquez/piano-roll" target="_blank">Github</a>
+            <a href="https://github.com/brandonjmarquez/piano-roll" target="_blank">Github Repo</a>
           </div>
 
           <div className="project-container">
@@ -21,7 +21,7 @@ const Projects = () => {
             </a>
             <p className="subtext"><strong><a href="https://brandonjmarquez.github.io/Website/">This Website</a></strong></p>
             <p className="proj-desc">My website, made with TypeScript and React.js. </p>
-            <a href="https://github.com/brandonjmarquez/Website" target="_blank">Github</a>
+            <a href="https://github.com/brandonjmarquez/Website" target="_blank">Github Repo</a>
           </div>
 
           <div className="project-container">
@@ -30,7 +30,16 @@ const Projects = () => {
             </a>
             <p className="subtext"><strong><a href="https://car-dealership-woad.vercel.app" target="_blank">Car Dealership Website</a></strong></p>
             <p className="proj-desc">A mock car dealership website. Made with TypeScript and React.js. Styled with TalwindCSS and Emotion.</p>
-            <a href="https://github.com/brandonjmarquez/car-dealership">Github</a>
+            <a href="https://github.com/brandonjmarquez/car-dealership">Github Repo</a>
+          </div>
+
+          <div className="project-container">
+            <a href="https://blab-times.vercel.app/" target="_blank">
+              <img src="/Website/projects/blab-times.png" className="project-pic" loading="lazy"></img>
+            </a>
+            <p className="subtext"><strong><a href="https://blab-times.vercel.app/" target="_blank">Blab Times Blog</a></strong></p>
+            <p className="proj-desc">A blog I made for my girlfriend! Made with TypeScript, React.js, Astro, Strapi, and TalwindCSS.</p>
+            <a href="https://github.com/brandonjmarquez/blab-times">Github Repo</a>
           </div>
 
           <div className="project-container">
